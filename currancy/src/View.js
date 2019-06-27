0a216add70f8918c20ea90286cc6e7e19f312d49
DMihail <dzezelomihail@gmail.com>
2019-06-28 01:43:41 +0300

    true fetch

--- currancy/src/View.js
+++ currancy/src/View.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import style from './viev.css'
-import bitcoin from './img/bitcoin.png';
-import litecoin from './img/litecoin.png';
-import ethereum from  './img/Ethereum.png';
+
 const URL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+
 class View extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             Price: undefined,
             Percent: undefined,
@@ -20,6 +18,9 @@ class View extends React.Component {
         this.Percent = this.Percent.bind(this);
     }
 
+    Table = [];
+    massName = ['Price : ', 'Percent change : ', 'Hour change : ', 'Day change : ', 'Week change : ', 'Month change : '];
+    massData = [this.state.Price, this.state.Percent, this.state.Hour, this.state.Day, this.state.Week, this.state.Month];
 
     Percent(event){
             event.preventDefault();
@@ -27,7 +28,7 @@ class View extends React.Component {
         this.setState({percent : event.target.value});
     }
 
-        getData = async (name, value) =>{
+    getData = async (name, value) => {
           const response = await fetch(URL + name + value );
               const data =  await response.json();
               console.log(data);
@@ -40,30 +41,32 @@ class View extends React.Component {
             });
     };
 
+    CreateTable(){
+        for (let i = 0; i < this.massName.length; i++){
+            if (this.massData[i] < 0){
+                this.Table.push(<tr><td className = 'data' style={style}>{this.massName[i]}</td><td className = 'negative' style={style}>{this.massData[i]}</td></tr>)
+            }else {
+                this.Table.push(<tr><td className = 'data' style={style}>{this.massName[i]}</td><td className = 'positive' style={style}>{this.massData[i]}</td></tr>)
+            }
+        }
+    }
+
+
     render() {
         this.getData(this.props.name, this.props.currency);
-        //console.log(this.props.name);
-        //console.log(this.props.currency);
-        let img;
+       // let img;
        switch (this.props.name) {
            case 'BTC':
-               img = bitcoin;
+             //  img = bitcoin;
                 break;
            case 'LTC':
-               img = litecoin;
+             //  img = litecoin;
                break;
            case 'ETH':
-               img = ethereum;
+             //  img = ethereum;
                break;
        }
-//  <img src={img} alt="" style={{position: 'absolute'}}/>
-        return (
-          <div>
-
-                <div className={'img_data'}>
-              <table>
-                  <tbody>
-                  <tr><td className={'Price'} style={style}>Price :</td><td className={'Pdata'} style={style}>{this.state.Price}</td></tr>
+       /* <tr><td className={'Price'} style={style}>Price :</td><td className={'Pdata'} style={style}>{this.state.Price}</td></tr>
                       <tr ><td className={'Percent_change'} style={style}>Percent change</td><td>
                           <input type="checkbox" name={'percent'} onChange={this.Percent} value={this.state.percent}/>
                       </td></tr>
@@ -71,6 +74,15 @@ class View extends React.Component {
                         <tr><td className={'Other'} style={style}>Day change</td><td className={'data'} style={style}>{this.state.Day}</td></tr>
                       <tr><td className={'Other'} style={style}>Week change</td><td className={'data'} style={style}>{this.state.Week}</td></tr>
                   <tr><td className={'Other'} style={style}>Month change</td><td className={'data'} style={style}>{this.state.Month}</td></tr>
+                 */
+//  <img src={img} alt="" style={{position: 'absolute'}}/>
+        this.CreateTable();
+        return (
+          <div>
+                <div className={'img_data'}>
+              <table>
+                  <tbody>
+                  {this.Table}
                   </tbody>
               </table>
                 </div>
