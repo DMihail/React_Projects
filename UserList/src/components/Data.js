7c16ab2cbbf131e545eca3051093cabc55885764
Michail <dzezelomihail@gmail.com>
2019-07-02 22:41:27 +0300

    add

--- UserList/src/components/Data.js
+++ UserList/src/components/Data.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import client from './clients.json';
+
+
+class Data extends React.Component {
   // constructor() {
+  //
+  // }
+   AddList(){
+
+           <p>FirstName:  {client[key]['general']['firstName']}</p>
+           <p>LastName:  {client[key]['general']['lastName']}</p>
+           <img src = {client[key]['general']['avatar']} />
+
+      
+      return MassList;
+  }
+
+  render(){
+
+    return(
+        <div>
+
+        </div>
+
+    )
+  }
+}
+
+export default Data;
--- UserList/src/components/List.js
+++ UserList/src/components/List.js
@@ -1,1 +1,36 @@
+import React from 'react';
+import client from './clients.json';
+
+
+class List extends React.Component {
+  // constructor() {
+  //
+  // }
+   AddList(){
+     let MassList = [];
+    for (var key in client) {
          MassList.push(<li>
+           <p>FirstName:  {client[key]['general']['firstName']}</p>
+           <p>LastName:  {client[key]['general']['lastName']}</p>
+           <img src = {client[key]['general']['avatar']} />
+          </li>)
+      }
+      return MassList;
+  }
+
+  render(){
+
+    return(
+        <div>
+
+          <ul>
+          { this.AddList()}
+          </ul>
+
+        </div>
+
+    )
+  }
+}
+
+export default List;
